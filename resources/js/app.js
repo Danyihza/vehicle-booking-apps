e36ebfd12d1fcf4f7b5c837b9a0f67f5e63aed9d
Danyihza <danyahmadihza99@gmail.com>
2022-09-01 22:04:35 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+import './bootstrap';
+import './charts';
+import './sidebar';
+
---
+++ resources/views/admin/_layouts/master.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html data-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Aplikasi Pemesanan Kendaraan">
+
+    <title>@yield('title') | Aplikasi Pemesanan Kendaraan</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <link rel="apple-touch-icon" sizes="180x180" href="https://demo.themesberg.com/windster/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://demo.themesberg.com/windster/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://demo.themesberg.com/windster/favicon-16x16.png">
+    <link rel="icon" type="image/png" href="https://demo.themesberg.com/windster/favicon.ico">
+
+
+</head>
+
+<body class="bg-gray-50">
+    @include('admin._layouts.topbar')
+    <div class="flex overflow-hidden bg-white pt-16">
+
+        @include('admin._layouts.sidebar')
+
+        <div id="main-content" class="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
+            @yield('content')
+            @include('admin._layouts.bottombar')
+        </div>
+
+    </div>
+
+</body>
+
+</html>
